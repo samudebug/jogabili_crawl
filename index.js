52aaaea0d87a1ee8b7ee03099e7e9da1cb301b30
Samuel Martins <samuel.ubuntu@gmail.com>
2021-08-09 12:27:00 -0300

    Set custom host

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const router = require('./src/routes')
 const app = express()
+const http = require('http');
 
 app.use(express.json())
 app.use('/', router)
+const server = http.createServer(app);
 
 const port = process.env.PORT || 3000
 const host = process.env.HOST || "localhost";
 
-app.listen(port, host)
-app.on("listening", () => {
+server.listen(port, host)
+server.on("listening", () => {
     console.log('Express server started on port %s at %s', server.address().port, server.address().address);
 })
