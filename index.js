6fb9a07ec50bc014fb3f5e79f379d3ee144b22b6
Samuel Martins <samuel.ubuntu@gmail.com>
2021-08-09 12:33:33 -0300

    Set custom host

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const router = require('./src/routes')
 const app = express()
 const http = require('http');
+const morgan = require('morgan');
 
+app.use(morgan('combined'));
 app.use(express.json())
 app.use('/', router)
+
 const server = http.createServer(app);
 
 const port = process.env.PORT || 3000
